64dd3bde1a7be52603b2db579a04ae14ca4a0347
KoshiOsaki <osaki36dg@gmail.com>
2024-03-05 02:35:51 +0900

    zenkouに投げ銭を紐付け

--- functions/src/function/others/postPoint.ts
+++ functions/src/function/others/postPoint.ts
@@ -1,22 +1,61 @@
-import { WebClient } from '@slack/web-api';
+import { ChatPostEphemeralArguments, WebClient } from '@slack/web-api';
+
+import { DOKOKICHI_WORKSPACE_ID } from '../../constants';
+import { User } from '../../types/models/user';
+import { DonatedPoint, Zenkou } from '../../types/models/zenkou';
+import { findUser, updateUser } from '../repository/user';
+import { findZenkou, updateZenkou } from '../repository/zenkou';
 
 export const postPoint = async (
   slackWebClient: WebClient,
   channelId: string,
   amount: number,
   postUserId: string,
+  zenkouUserId: string,
   zenkouId: string,
 ) => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  await slackWebClient.chat.postEphemeral({
+  let postUser = await findUser(DOKOKICHI_WORKSPACE_ID, postUserId);
+  // selectedUserのドキュメントが存在しなければ新規作成し、200 zen付与する
+  if (!postUser) {
+    const newUser: User = {
+      id: postUserId,
+      availablePoint: 200,
+      isDeleted: false,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+    await updateUser(DOKOKICHI_WORKSPACE_ID, newUser);
+    postUser = newUser;
+  }
+
+  // メッセージの内容を設定
+  const postEphemeral: ChatPostEphemeralArguments = {
     channel: channelId,
     user: postUserId, // エフェメラルメッセージを表示するユーザーのID
-    text: '10zen 投げ銭しました。今週はあと90zenです。',
-  });
-
-  // postUserのdocumentが存在しなければドキュメントを新規作成し、200 zen付与
+    text: `${amount}zen 投げ銭しました。今週はあと${postUser.availablePoint - amount}zenです。`,
+  };
+  // あなただけに表示されるメッセージを送信
+  await slackWebClient.chat.postEphemeral(postEphemeral);
 
   // postUserからzenを引く
+  const updatedUser: User = {
+    ...postUser,
+    availablePoint: postUser.availablePoint - amount,
+  };
+  await updateUser(DOKOKICHI_WORKSPACE_ID, updatedUser);
 
   // zenkouにzenを加算
+  const zenkou = await findZenkou(DOKOKICHI_WORKSPACE_ID, zenkouUserId, zenkouId);
+  if (!zenkou) throw new Error('zenkou not found');
+
+  const newDonatedPoint: DonatedPoint = {
+    userId: postUserId,
+    point: amount,
+  };
+  const donatedPointList = [...zenkou.donatedPointList, newDonatedPoint];
+  const updatedZenkou: Zenkou = {
+    ...zenkou,
+    donatedPointList: donatedPointList,
+  };
+  await updateZenkou(DOKOKICHI_WORKSPACE_ID, zenkouUserId, updatedZenkou);
 };
--- functions/src/function/others/sendZenkouForm.ts
+++ functions/src/function/others/sendZenkouForm.ts
@@ -1,6 +1,12 @@
 import { ChatPostMessageArguments, WebClient } from '@slack/web-api';
 import * as functions from 'firebase-functions';
 
+import { DOKOKICHI_WORKSPACE_ID } from '../../constants';
+import { User } from '../../types/models/user';
+import { Zenkou } from '../../types/models/zenkou';
+import { findUser, updateUser } from '../repository/user';
+import { updateZenkou } from '../repository/zenkou';
+
 // 善行送信モーダルのsendを押したときの処理
 export const sendZenkouForm = async (
   slackWebClient: WebClient,
@@ -11,18 +17,38 @@ export const sendZenkouForm = async (
 ): Promise<void> => {
   // モーダルの送信イベントの処理
 
-  // TODO: selectedUserのドキュメントが存在しなければ新規作成し、200 zen付与する処理を書く
-  // TODO: dbのzenkouコレクションにデータを追加する処理を書く
+  const selectedUser = await findUser(DOKOKICHI_WORKSPACE_ID, selectedUserId);
+  // selectedUserのドキュメントが存在しなければ新規作成し、200 zen付与する
+  if (!selectedUser) {
+    const newUser: User = {
+      id: selectedUserId,
+      availablePoint: 200,
+      isDeleted: false,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+    await updateUser(DOKOKICHI_WORKSPACE_ID, newUser);
+  }
 
+  // メッセージの内容を設定
   const postMessage: ChatPostMessageArguments = {
     channel: channelId,
     text: `<@${selectedUserId}> さんの善行が投稿されたよ！\n${userInput}\n\nzenを投げてあげよう！`, // プレーンテキストとしてのフォールバックテキスト
     blocks: [
       {
+        type: 'context',
+        elements: [
+          {
+            type: 'mrkdwn',
+            text: `<@${selectedUserId}> さんの善行が投稿されたよ！`,
+          },
+        ],
+      },
+      {
         type: 'section',
         text: {
           type: 'mrkdwn',
-          text: `<@${selectedUserId}> さんの善行が投稿されたよ！\n${userInput}`, // ブロック内のテキストメッセージ
+          text: userInput, // ブロック内のテキストメッセージ
         },
       },
       {
@@ -74,5 +100,18 @@ export const sendZenkouForm = async (
   };
 
   // チャンネルにメッセージを投稿
-  await slackWebClient.chat.postMessage(postMessage);
+  const response = await slackWebClient.chat.postMessage(postMessage);
+  const messageTs = response.ts;
+  if (!messageTs) throw new Error('messageTs is undefined');
+
+  // dbのzenkouコレクションにデータを追加する処理を書く
+  const zenkou: Zenkou = {
+    id: messageTs,
+    content: userInput,
+    donatedPointList: [],
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+    isDeleted: false,
+  };
+  await updateZenkou(DOKOKICHI_WORKSPACE_ID, selectedUserId, zenkou);
 };
