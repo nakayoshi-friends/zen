e7101b9f417d94747eedda8db4920365d99c3e5f
KoshiOsaki <osaki36dg@gmail.com>
2024-03-17 01:30:35 +0900

    アクセストークンを使用するように修正

--- functions/src/api/displayModal.ts
+++ functions/src/api/displayModal.ts
@@ -2,24 +2,36 @@ import { WebClient } from '@slack/web-api';
 import * as functions from 'firebase-functions';
 
 import { displayPostModal } from '../function/others/displayPostModal';
+import { findWorkspace } from '../function/repository/workspace';
 
 // /zen で呼び出されるエンドポイント
 export const displayModal = functions.region('asia-northeast1').https.onRequest(async (req, res) => {
-  // Bot User OAuth Access Tokenを設定
-  const slackWebClient = new WebClient(process.env.SLACK_TOKEN);
-
-  // request bodyから情報を取得
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  const channelId = req.body.channel_id as string;
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  const triggerId = req.body.trigger_id as string;
-  console.log(req.body); // デバッグ用
-
   try {
-    await displayPostModal(slackWebClient, triggerId, channelId);
-    res.status(200).send('OK');
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const workspaceId = req.body.team_id as string;
+    // ワークスペースIDからアクセストークンを取得
+    // ワークスペースIDからアクセストークンを取得
+    const _workspace = await findWorkspace(workspaceId);
+    if (!_workspace) throw new Error('workspace not found');
+    const accessToken = _workspace.accessToken;
+    const slackWebClient = new WebClient(accessToken);
+
+    // request bodyから情報を取得
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const channelId = req.body.channel_id as string;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const triggerId = req.body.trigger_id as string;
+    console.log(req.body); // デバッグ用
+
+    try {
+      await displayPostModal(slackWebClient, triggerId, channelId);
+      res.status(200).send('OK');
+    } catch (e) {
+      console.error('Error display modal: ', e);
+      res.status(500).send('Error display modal');
+    }
   } catch (e) {
-    console.error('Error display modal: ', e);
-    res.status(500).send('Error display modal');
+    console.error('Error: ', e);
+    res.status(500).send('Error');
   }
 });
--- functions/src/api/slackInteraction.ts
+++ functions/src/api/slackInteraction.ts
@@ -4,100 +4,108 @@ import * as functions from 'firebase-functions';
 
 import { postPoint } from '../function/others/postPoint';
 import { sendZenkouForm } from '../function/others/sendZenkouForm';
+import { findWorkspace } from '../function/repository/workspace';
 
 // slack上のアクション時呼ばれるエンドポイント
 export const slackInteraction = functions.region('asia-northeast1').https.onRequest(async (req, res) => {
-  // Bot User OAuth Access Tokenを設定
-  const slackWebClient = new WebClient(process.env.SLACK_TOKEN);
+  try {
+    // リクエストのペイロードを解析する
+    // FIXME: eslint-disableを外す
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument
+    const payload = JSON.parse(req.body.payload || '{}');
+    console.log('payload:', payload); // デバッグ用
 
-  // リクエストのペイロードを解析する
-  // FIXME: eslint-disableを外す
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument
-  const payload = JSON.parse(req.body.payload || '{}');
+    const workspaceId = (payload.team?.id as string) ?? '';
+    // ワークスペースIDからアクセストークンを取得
 
-  console.log('payload:', payload); // デバッグ用
+    const _workspace = await findWorkspace(workspaceId);
+    if (!_workspace) throw new Error('workspace not found');
+    const accessToken = _workspace.accessToken;
+    const slackWebClient = new WebClient(accessToken);
 
-  const workspaceId = (payload.team?.id as string) ?? '';
-
-  // Slackからのイベントタイプによって処理を分岐
-  switch (payload.type) {
-    // モーダルの送信イベントを処理
-    case 'view_submission': {
-      try {
-        const selectedUserId = payload.view.state.values['zen_user']['users_select-action']?.selected_user as string;
-        const channelId = payload.view.state.values['posted_channel']['channel_input'].value as string;
-        const userInput = payload.view.state.values['zen_content']['plain_text_input-action'].value as string;
-        await sendZenkouForm(slackWebClient, selectedUserId, userInput, workspaceId, channelId, res);
-        // 必要な処理を行った後、正常に処理されたことをSlackに通知
-        res.json({ response_action: 'clear' });
-      } catch (e) {
-        console.error('Error posting message: ', e);
-        res.status(500).send('Error posting message to Slack');
+    // Slackからのイベントタイプによって処理を分岐
+    switch (payload.type) {
+      // モーダルの送信イベントを処理
+      case 'view_submission': {
+        try {
+          const selectedUserId = payload.view.state.values['zen_user']['users_select-action']?.selected_user as string;
+          const channelId = payload.view.state.values['posted_channel']['channel_input'].value as string;
+          const userInput = payload.view.state.values['zen_content']['plain_text_input-action'].value as string;
+          await sendZenkouForm(slackWebClient, selectedUserId, userInput, workspaceId, channelId, res);
+          // 必要な処理を行った後、正常に処理されたことをSlackに通知
+          res.json({ response_action: 'clear' });
+        } catch (e) {
+          console.error('Error posting message: ', e);
+          res.status(500).send('Error posting message to Slack');
+        }
+        break;
       }
-      break;
-    }
 
-    // ボタンのクリックイベントを処理
-    case 'block_actions': {
-      // action_idによって処理を分岐
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      const channelId = (payload.channel?.id as string) ?? '';
-      const postedUserId = payload.user.id as string;
-      const messageTs = (payload.message?.ts as string) ?? ''; // zenkouIdにあたる
-      const text = (payload.message?.text as string) ?? '';
-      const zenkouUserId = text.match(/<@(\w+)>/)?.[1] ?? ''; // 正規表現を使用してテキストからユーザーIDを抽出
+      // ボタンのクリックイベントを処理
+      case 'block_actions': {
+        // action_idによって処理を分岐
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const channelId = (payload.channel?.id as string) ?? '';
+        const postedUserId = payload.user.id as string;
+        const messageTs = (payload.message?.ts as string) ?? ''; // zenkouIdにあたる
+        const text = (payload.message?.text as string) ?? '';
+        const zenkouUserId = text.match(/<@(\w+)>/)?.[1] ?? ''; // 正規表現を使用してテキストからユーザーIDを抽出
 
-      switch (payload.actions[0].action_id) {
-        // zenを投げるボタンを押したときの処理
-        case 'action-10':
-          await postPoint(slackWebClient, workspaceId, channelId, 10, postedUserId, zenkouUserId, messageTs);
-          res.status(200).send('OK');
-          break;
-        case 'action-20':
-          await postPoint(slackWebClient, workspaceId, channelId, 20, postedUserId, zenkouUserId, messageTs);
-          res.status(200).send('OK');
-          break;
-        case 'action-30':
-          await postPoint(slackWebClient, workspaceId, channelId, 30, postedUserId, zenkouUserId, messageTs);
-          res.status(200).send('OK');
-          break;
-        // モーダルでユーザーを選択したときの処理
-        case 'users_select-action':
-          // 何もしない
-          res.status(200).send('OK');
-          break;
-        default:
-          console.log('未知のアクションID:', payload.actions[0].action_id);
-          res.status(400).send('未知のアクションIDです');
+        switch (payload.actions[0].action_id) {
+          // zenを投げるボタンを押したときの処理
+          case 'action-10':
+            await postPoint(slackWebClient, workspaceId, channelId, 10, postedUserId, zenkouUserId, messageTs);
+            res.status(200).send('OK');
+            break;
+          case 'action-20':
+            await postPoint(slackWebClient, workspaceId, channelId, 20, postedUserId, zenkouUserId, messageTs);
+            res.status(200).send('OK');
+            break;
+          case 'action-30':
+            await postPoint(slackWebClient, workspaceId, channelId, 30, postedUserId, zenkouUserId, messageTs);
+            res.status(200).send('OK');
+            break;
+          // モーダルでユーザーを選択したときの処理
+          case 'users_select-action':
+            // 何もしない
+            res.status(200).send('OK');
+            break;
+          default:
+            console.log('未知のアクションID:', payload.actions[0].action_id);
+            res.status(400).send('未知のアクションIDです');
+        }
+        break;
       }
-      break;
-    }
 
-    case 'message_action': {
-      // 多分不要
-      try {
-        console.log('message_actionを処理');
-      } catch (e) {
-        console.error('Error display modal: ', e);
-        res.status(500).send('Error display modal');
+      case 'message_action': {
+        // 多分不要
+        try {
+          console.log('message_actionを処理');
+        } catch (e) {
+          console.error('Error display modal: ', e);
+          res.status(500).send('Error display modal');
+        }
+        break;
       }
-      break;
-    }
 
-    case 'shortcut': {
-      // 多分不要
-      try {
-        console.log('shortcutを処理');
-      } catch (e) {
-        console.error('Error display modal: ', e);
-        res.status(500).send('Error display modal');
+      case 'shortcut': {
+        // 多分不要
+        try {
+          console.log('shortcutを処理');
+        } catch (e) {
+          console.error('Error display modal: ', e);
+          res.status(500).send('Error display modal');
+        }
+        break;
       }
-      break;
-    }
 
-    default:
-      // 予期しないイベントタイプの場合
-      console.log('未知のイベントタイプ:', payload.type);
-      res.status(400).send('未知のイベントタイプです');
+      default:
+        // 予期しないイベントタイプの場合
+        console.log('未知のイベントタイプ:', payload.type);
+        res.status(400).send('未知のイベントタイプです');
+    }
+  } catch (e) {
+    console.error('Error: workspace not found');
+    return;
   }
 });
